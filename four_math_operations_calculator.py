38c13245d05331915106767063f982973e2cbabf
ylla-b-28 <ylla.briza28@gmail.com>
2026-05-02 01:10:52 +0800

    I added fixed the indentation errors.

--- four_math_operations_calculator.py
+++ four_math_operations_calculator.py
@@ -1,18 +1,34 @@
 class MathOperationsCalculator:
 
-def add_numbers(self, first_number, second_number):
-    return first_number + second_number
+    def add_numbers(self, first_number, second_number):
+       return first_number + second_number
 
-def subtract_numbers(self, first_number, second_number):
-    return first_number - second_number
+    def subtract_numbers(self, first_number, second_number):
+       return first_number - second_number
 
-def multiply_numbers(self, first_number, second_number):
-    return first_number * second_number
+    def multiply_numbers(self, first_number, second_number):
+       return first_number * second_number
 
-def divide_numbers(self, first_number, second_number):
-    return first_number / second_number
+    def divide_numbers(self, first_number, second_number):
+       return first_number / second_number
 
-#operation
+class AppCalculator:
+    def start_application(self):
+        print("Application is starting...")
 
-#input
-#ouput
+def start_application(self):
+    while True:
+        print("\nSelect: 1. Add | 2. Subtract | 3. Multiply | 4. Divide")
+        user_choice = input("Your choice: ")
+
+        first_value = float(input("Enter first number: "))
+        second_value = float(input("Enter second number: "))
+
+        if user_choice == '1':
+            result = self.add_numbers(first_value, second_value)
+            print(f"Result: {result}")
+
+        repeat = input("Try again? (Yes/No): ").lower()
+        if repeat != 'Yes':
+            print("\nThank you for using the calculator!")
+            break
