fb4421f88e7acf0af3e9bc36ab68d3a28a449e46
ylla-b-28 <ylla.briza28@gmail.com>
2026-05-02 00:52:40 +0800

    I added the math operations.

--- four_math_operations_calculator.py
+++ four_math_operations_calculator.py
@@ -1,12 +1,16 @@
 class MathOperationsCalculator:
 
 def add_numbers(self, first_number, second_number):
+    return first_number + second_number
 
 def subtract_numbers(self, first_number, second_number):
+    return first_number - second_number
 
 def multiply_numbers(self, first_number, second_number):
+    return first_number * second_number
 
 def divide_numbers(self, first_number, second_number):
+    return first_number / second_number
 
 #operation
 
