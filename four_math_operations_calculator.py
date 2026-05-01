7fa4685dd2978dce67f1a4652ffe1915de5237c0
ylla-b-28 <ylla.briza28@gmail.com>
2026-05-02 00:51:36 +0800

    I added the variable names.

--- four_math_operations_calculator.py
+++ four_math_operations_calculator.py
@@ -1,6 +1,13 @@
-#class
+class MathOperationsCalculator:
 
-#variable
+def add_numbers(self, first_number, second_number):
+
+def subtract_numbers(self, first_number, second_number):
+
+def multiply_numbers(self, first_number, second_number):
+
+def divide_numbers(self, first_number, second_number):
+
 #operation
 
 #input
